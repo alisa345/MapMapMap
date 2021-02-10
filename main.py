f6bd3fe6eb0dd9e05833fb2fab0e4faea3644f5c
0-122-5 <0-122-5>
2021-02-10 17:19:47 +0500

    exe 3

--- main.py
+++ main.py
@@ -14,17 +14,36 @@ class MyWidget(QMainWindow):
         self.button_find.clicked.connect(self.find)
         self.edit_longitude.setText('60.58985462732907')
         self.edit_latitude.setText('56.84822763650701')
+        self.lt_cent = self.edit_latitude.text()
+        self.ln_cent = self.edit_longitude.text()
+        self.find()
 
         def keys_listener(e):
             if e.name == 'page up' and e.event_type == 'up':
                 self.chande_value(1)
             elif e.name == 'page down' and e.event_type == 'up':
                 self.chande_value(2)
+            elif e.name == 'up' and e.event_type == 'up':
+                if float(self.lt_cent) - 0.001 <= 90 and float(self.lt_cent) - 0.001 >= -90:
+                    self.lt_cent = str(float(self.lt_cent) + 0.001)
+                self.find(True)
+            elif e.name == 'down' and e.event_type == 'up':
+                if float(self.lt_cent) - 0.001 <= 90 and float(self.lt_cent) - 0.001 >= -90:
+                    self.lt_cent = str(float(self.lt_cent) - 0.001)
+                self.find(True)
+            elif e.name == 'right' and e.event_type == 'up':
+                if float(self.ln_cent) - 0.001 <= 180 and float(self.ln_cent) - 0.001 >= -180:
+                    self.ln_cent = str(float(self.ln_cent) + 0.001)
+                self.find(True)
+            elif e.name == 'left' and e.event_type == 'up':
+                if float(self.ln_cent) - 0.001 <= 180 and float(self.ln_cent) - 0.001 >= -180:
+                    self.ln_cent = str(float(self.ln_cent) - 0.001)
+                self.find(True)
 
         keyboard.hook(keys_listener)
 
-    def find(self):
-        self.requests_api()
+    def find(self, press=False):
+        self.requests_api(press)
         map_photo = QPixmap(self.map_pic)
         map_photo = map_photo.scaled(1051, 751)
         self.map_lable.setPixmap(map_photo)
@@ -36,15 +55,19 @@ class MyWidget(QMainWindow):
             self.scale_spin_box.setValue(self.scale_spin_box.value() - 0.01)
         self.find()
 
-    def requests_api(self):
+    def requests_api(self, press):
         lat = self.edit_latitude.text()
         lon = self.edit_longitude.text()
+        if press is False:
+            self.lt_cent = self.edit_latitude.text()
+            self.ln_cent = self.edit_longitude.text()
         scale = str(self.scale_spin_box.value())
         api_server = "http://static-maps.yandex.ru/1.x/"
 
         params = {
             "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-            "ll": ",".join([lon, lat]),
+            "sll": ",".join([lon, lat]),
+            "ll": ",".join([self.ln_cent, self.lt_cent]),
             "spn": ",".join([scale, scale]),
             "l": "map"
         }
