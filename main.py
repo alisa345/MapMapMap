9851eda0a05ed4203492da9f3e1a90c52c652ea0
0-122-5 <0-122-5>
2021-02-10 18:01:56 +0500

    omg not working

--- main.py
+++ main.py
@@ -25,19 +25,19 @@ class MyWidget(QMainWindow):
                 self.chande_value(2)
             elif e.name == 'up' and e.event_type == 'up':
                 if float(self.lt_cent) - 0.001 <= 90 and float(self.lt_cent) - 0.001 >= -90:
-                    self.lt_cent = str(float(self.lt_cent) + 0.001)
+                    self.lt_cent = str(float(self.lt_cent) + 0.0091)
                 self.find(True)
             elif e.name == 'down' and e.event_type == 'up':
                 if float(self.lt_cent) - 0.001 <= 90 and float(self.lt_cent) - 0.001 >= -90:
-                    self.lt_cent = str(float(self.lt_cent) - 0.001)
+                    self.lt_cent = str(float(self.lt_cent) - 0.0091)
                 self.find(True)
             elif e.name == 'right' and e.event_type == 'up':
                 if float(self.ln_cent) - 0.001 <= 180 and float(self.ln_cent) - 0.001 >= -180:
-                    self.ln_cent = str(float(self.ln_cent) + 0.001)
+                    self.ln_cent = str(float(self.ln_cent) + 0.015)
                 self.find(True)
             elif e.name == 'left' and e.event_type == 'up':
                 if float(self.ln_cent) - 0.001 <= 180 and float(self.ln_cent) - 0.001 >= -180:
-                    self.ln_cent = str(float(self.ln_cent) - 0.001)
+                    self.ln_cent = str(float(self.ln_cent) - 0.015)
                 self.find(True)
 
         keyboard.hook(keys_listener)
@@ -56,6 +56,8 @@ class MyWidget(QMainWindow):
         self.find()
 
     def requests_api(self, press):
+        view_map = self.view_combobox.currentText()
+        print(view_map)
         lat = self.edit_latitude.text()
         lon = self.edit_longitude.text()
         if press is False:
@@ -63,13 +65,19 @@ class MyWidget(QMainWindow):
             self.ln_cent = self.edit_longitude.text()
         scale = str(self.scale_spin_box.value())
         api_server = "http://static-maps.yandex.ru/1.x/"
+        if view_map == 'Схема':
+            type = 'map'
+        elif view_map == 'Спутник':
+            type = 'sat'
+        else:
+            type = 'sat,skl'
 
         params = {
             "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
             "sll": ",".join([lon, lat]),
             "ll": ",".join([self.ln_cent, self.lt_cent]),
             "spn": ",".join([scale, scale]),
-            "l": "map"
+            "l": type
         }
 
         response = requests.get(api_server, params=params)
